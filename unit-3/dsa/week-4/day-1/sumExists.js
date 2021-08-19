67b5474d22abba385a64fc7e90a7446bbf924316
Ananthu Asokkumar <ananthuasokkumar@gmail.com>
2021-08-19 13:25:41 +0530

    sum dsa

--- unit-3/dsa/week-4/day-1/sumExists.js
+++ unit-3/dsa/week-4/day-1/sumExists.js
@@ -1,41 +1,73 @@
-function check(arr,start,end,res,sum) {
-    if (start <= end + 1 && res.length!=0) {
-        var s = 0;
-        console.log(res);
-        
-        for (let i = 0; i < res.length; i++){
-            s += res[i];
-        }
-        if (s == sum)
-            return console.log('yes');
-        else {
-            if (start == end && s != sum)
-                return console.log('no');
-        }
-            
-            
+// Description
+
+// Given an array of non-negative integers, and a value 'sum', determine if there is a subset of the given set (array) with sum equal to given sum.
+
+// If there is a subset whose sum is equal to the required sum then print "yes" else print "no" without quotes.
+
+
+// Input
+// Input Format :
+
+// The first line of input contains an integer N - denoting size of the array.
+
+// The second line contains the N space seperated integers.
+
+// The third line of input will contain S - the required Sum value.
+
+// Constraints :
+
+// 1<=n<= 18
+
+// 1<=A[i]<=10^9
+
+
+// Output
+// Print "yes" or "no" without quotes.
+
+
+// Sample Input 1 
+
+// 9
+// 1 2 3 4 5 6 7 8 9
+// 5
+// Sample Output 1
+
+// yes
+
+
+function subSet(arr, n, sum) {
+    if (sum == 0) {
+        return true;
     }
-    for (var i = start; i <= end; i++){
-        res.push(arr[i])
-        check(arr,i+1,end,res,sum)
-   }
- check(arr,start+1,end,[],sum)
+    if (n == 0) {
+        return false;
+    }
+    if (arr[n - 1] > sum) {
+        return subSet(arr, n - 1, sum);
+    }
+    else {
+        return subSet(arr, n - 1, sum) || subSet(arr, n - 1, sum - arr[n - 1]);
+    }
 }
 
-
-
-
 function runProgram(input) {
-    var newInput = input.split("\n");
-    var arr = newInput[1].trim().split(" ").map(Number);
-    var sum = +newInput[2];
-    var newArr = [];
-    console.log(sum);
-    
+    input = input.trim().split("\n");
+    // console.log(input);
+    let n = +input[0];
+    let arr = input[1].trim().split(" ");
+    let sum = +input[2];
 
-    check(arr,0,arr.length-1,newArr,sum)
-       
- }
+    // console.log(arr);
+    // console.log(sum);
+
+    if (subSet(arr, n, sum)) {
+        console.log("yes");
+    }
+    else {
+        console.log("no");
+    }
+    
+}
 if (process.env.USERNAME === "Ananthu Asokkumar") {
     runProgram(`9
 1 2 3 4 5 6 7 8 9
